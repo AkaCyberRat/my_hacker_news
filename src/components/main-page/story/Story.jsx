d69abedb82533dc9fe354663a923d242ac5e5d5b
AkaCyberRat <andrejchuk01@gmail.com>
2023-03-30 23:12:53 +0700

    Prepare Story component to data receiving

--- src/components/main-page/story/Story.jsx
+++ src/components/main-page/story/Story.jsx
@@ -1,21 +1,21 @@
 
-const Story = () => {
+const Story = ({story}) => {
     return (
         <div className="story p-1 container-flux border-bottom border-2 border-secondary bg-light">
             <div className="row">
                 <div className="col ps-4">
-                    <div className="text-left float-left">Title</div>
+                    <div className="text-left float-left">{story.title}</div>
                 </div>
                 <div className="col-4">
-                    <div className="text-left float-left">by Author</div>
+                    <div className="text-left float-left">by {story.by}</div>
                 </div>
             </div>
             <div className="row">
                 <div className="col ps-4">
-                    <div className="text-left float-left text-muted">222 points</div>
+                    <div className="text-left float-left text-muted">{story.score} point(s)</div>
                 </div>
                 <div className="col-4">
-                    <div className="text-left float-left text-muted"> 3 min ago</div>
+                    <div className="text-left float-left text-muted">{new Date(story.time * 1000).toLocaleString()}</div>
                 </div>
             </div>
         </div>
