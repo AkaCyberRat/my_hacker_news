a843e42b0ee71e7f4f8bdf91c315f2f108910af9
AkaCyberRat <andrejchuk01@gmail.com>
2023-04-01 17:49:18 +0700

    Add Story route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Header from "./components/header/Header";
 import MainPage from "./components/main-page/MainPage";
+import StoryPage from "./components/story-page/StoryPage";
 
 function App() {
   return (
     <>
-
       <Header />
-
 
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/story/:id' element={<StoryPage />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </>
   );
