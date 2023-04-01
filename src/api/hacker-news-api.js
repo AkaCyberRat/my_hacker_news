b455ad50b3cd3f3fe6be3cb39b357d3b980d5b62
AkaCyberRat <andrejchuk01@gmail.com>
2023-04-01 17:03:57 +0700

    Edit api: add sort by date after fetch

--- src/api/hacker-news-api.js
+++ src/api/hacker-news-api.js
@@ -5,11 +5,12 @@ const API = {
         const response = await fetch(`https://hacker-news.firebaseio.com/v0/beststories.json?orderBy="$priority"&limitToFirst=${count}`)
         const storiesIds = await response.json()
 
-        const items = await API.getItemsByIds(storiesIds)
-        return items
+        const stories = await API.getItemsByIds(storiesIds)
+
+        return stories.sort((a, b) => (b.time - a.time))
     },
 
-    getItemsByIds: async (ids) => { 
+    getItemsByIds: async (ids) => {
 
         const promises = ids.map(id => {
             const promise = fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(r => r.json());
